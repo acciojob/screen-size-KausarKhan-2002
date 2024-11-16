e8f6ced5f49cbcd6fb927ab0e5889f84171bcdff
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-11-16 16:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let heading = document.querySelector("h1")
+
+window.addEventListener("load", () => {
+    heading.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+})
+
+window.addEventListener("resize", () => {
+    heading.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+})
